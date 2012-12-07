e60e1b4083609a21b7e50716c6b4db811d8e971a
Kim Merder <hoxnox@gmail.com>
2012-12-07 14:34:53 +0400

    int and ipv4 generators fixed

--- src/cursor_generator.cpp
+++ src/cursor_generator.cpp
@@ -23,11 +23,12 @@ namespace cursor {
  *                        const int repeat);
  * @endcode
  *
- * state - is a store of data, used to generate next value.
- *         Note that init argument of the cursor
- *         application is written to the state, so on first call of this
- *         function state can be not empty. Generator should write data for next
- *         call.
+ * state - is a store of data, used to generate next value. Note that initial
+ *         value must be returned too. So one approach to generator architecture is to
+ *         write "future value" to the state, rather than "current". Note also,
+ *         that the value of the init argument is written to the state, so on
+ *         first call of generator there may be initial value in the state.
+ *         Sometimes this initial value need preprocessing before returning out.
  * statesz - the size of the state parameter
  * statemaxsz - buffer size of the state parameter. The generator MUST NOT write
  *         over statemaxsz to the state.
@@ -42,12 +43,17 @@ namespace cursor {
  * To write your own generator, see examples below.*/
 
 
-/**@brief produces <init>, <init>+1, ...*/
+/**@brief produces <init>, <init>+1, ...
+ *
+ * We will write next int to the state buffer. Note that initial value will be
+ * in string representation! To distinct first call from others, we will write
+ * '0x02' byte to the state[0]. If the end of sequence reached, '0x03' will be
+ * written to the state[0].*/
 void simple_uint_generator(char* state, size_t* statesz, const size_t statemaxsz,
                            char * next, size_t* nextsz, const size_t nextmaxsz,
                            const int repeat)
 {
-	unsigned int init = 0;
+	unsigned int currv = 0;
 	unsigned int nextv = 0;
 	if(  state == NULL || statesz == NULL || statemaxsz == 0
 	  || next  == NULL || nextsz  == NULL || nextmaxsz  == 0
@@ -58,17 +64,49 @@ void simple_uint_generator(char* state, size_t* statesz, const size_t statemaxsz
 		*nextsz = 0;
 		return;
 	}
-	if(*statesz == 0)
-		init = 0;
-	else
+
+	// end of sequence
+	if(state[0] == 0x03)
 	{
-		char * init_c = (char*)malloc(*statesz + 1);
-		memset(init_c, 0, *statesz + 1);
-		memcpy(init_c, state, *statesz);
-		init = (unsigned)atoi(init_c);
-		free(init_c);
+		*nextsz = 0;
+		return;
 	}
-	if(init == std::numeric_limits<unsigned int>::max())
+
+	if(statesz == 0)
+	{
+		currv = 0;
+	}
+	else if(state[0] == 0x02)
+	{ // regular
+		if(*statesz < sizeof(unsigned int) + 1)
+		{
+			LOG(ERROR) << _("simple_uint_generator: state is too short.");
+			*nextsz = 0;
+			return;
+		}
+		currv = *((unsigned int*)&state[1]);
+	}
+	else
+	{ // initial
+		currv = (unsigned int)atol(state);
+	}
+
+	const int tmpsz = std::numeric_limits<unsigned int>::digits10 + 1;
+	char *tmp = (char *)malloc(tmpsz);
+	memset(tmp, 0, tmpsz);
+	sprintf(tmp, "%u", currv);
+	if(nextmaxsz < strlen(tmp))
+	{
+		LOG(ERROR) << _("simple_uint_generator: output buffer is too smal.");
+		*nextsz = 0;
+		return;
+	}
+	memset(next, 0, nextmaxsz);
+	memcpy(next, tmp, strlen(tmp));
+	*nextsz = strlen(tmp);
+
+	// prepare future
+	if(currv == std::numeric_limits<unsigned int>::max())
 	{
 		if(repeat)
 		{
@@ -77,35 +115,27 @@ void simple_uint_generator(char* state, size_t* statesz, const size_t statemaxsz
 		else
 		{
 			LOG(INFO) << _("simple_uint_generator: reached maximum value.");
-			*nextsz = 0;
+			state[0] = 0x03;
+			*statesz = 1;
 			return;
 		}
 	}
 	else
 	{
-		nextv = ++init;
+		nextv = ++currv;
 	}
-	const int tmpsz = std::numeric_limits<unsigned int>::digits10 + 1;
-	char *tmp = (char *)malloc(tmpsz);
-	memset(tmp, 0, tmpsz);
 
-	sprintf(tmp, "%d", nextv);
-	if(strlen(tmp) > nextmaxsz)
-	{
-		LOG(ERROR) << _("simple_uint_generator: next buffer is too small.");
-		nextsz = 0;
-		return;
-	}
-	*nextsz = strlen(tmp);
-	if(statemaxsz < *nextsz + 1)
+	if(statemaxsz < sizeof(unsigned int) + 1)
 	{
 		LOG(ERROR) << _("simple_uint_generator: state buffer is too small.");
 		*nextsz = 0;
 		return;
 	}
-	*statesz = *nextsz;
-	memcpy(next, tmp, *nextsz);
-	memcpy(state, tmp, *statesz);
+
+	memset(state, 0, statemaxsz);
+	state[0] = 0x02;
+	*((unsigned int*)&state[1]) = nextv;
+	*statesz = sizeof(unsigned int) + 1;
 }
 
 /**@brief produces ipv4 sequence (with port, if set)
@@ -115,53 +145,148 @@ void simple_uint_generator(char* state, size_t* statesz, const size_t statemaxsz
  *   "192.168.1.2:25"
  *    ...
  * Note that ip, that can't be set to a host in Internet would be skipped
- * (local, broadcast, reserved, ...) */
-void ipv4_generator(char* state, size_t* statesz, size_t* statemaxsz,
-                    char * next, size_t* nextsz, size_t* nextmaxsz,
+ * (local, broadcast, reserved, ...).*/
+void ipv4_generator(char* state, size_t* statesz, size_t statemaxsz,
+                    char * next, size_t* nextsz, size_t nextmaxsz,
                     int repeat)
 {
 	size_t colon_pos = 1;
-	struct sockaddr_in addr;
-	while(state[colon_pos] != ':' && colon_pos < *statesz)
-		++colon_pos;
-	// we don't need to convert in network byte order
-	addr.sin_port = (unsigned short)atoi(&state[colon_pos+1]);
-	state[colon_pos] = '\0';
-	*statesz = colon_pos;
-	if(inet_pton(AF_INET, &state[1], (struct sockaddr*)&addr) <= 0)
+	struct sockaddr_in* addr;
+	memset(&addr, 0, sizeof(addr));
+	if(  state == NULL || statesz == NULL || statemaxsz == 0
+	  || next  == NULL || nextsz  == NULL || nextmaxsz  == 0
+	  || *nextsz > nextmaxsz || *statesz > statemaxsz
+	  )
 	{
-		LOG(ERROR) << _("ipv4_generator: inet_pton error.") << " "
-			<< _("Message") << ": " << strerror(errno);
-		nextsz = 0;
+		LOG(ERROR) << _("ipv4_generator: wrong args.");
+		*nextsz = 0;
 		return;
 	}
 
+	// end of sequence
+	if(state[0] == 0x03)
+	{
+		*nextsz = 0;
+		return;
+	}
+
+	if(*statesz == 0)
+	{
+		if(statemaxsz < sizeof(sockaddr_in) + 1)
+ 		{
+			LOG(ERROR) << _("ipv4_generator: state buffer is too short.");
+			*nextsz = 0;
+			return;
+		}
+		state[0] = 0x02;
+		addr = (sockaddr_in*)&state[1];
+		memset(addr, 0, sizeof(sockaddr_in));
+		addr->sin_family = AF_INET;
+		addr->sin_addr.s_addr = inet_addr("1.0.0.1");
+	}
+	else if(state[0] == 0x02)
+	{ // regular
+		if(*statesz < sizeof(sockaddr_in) + 1)
+ 		{
+			LOG(ERROR) << _("ipv4_generator: state is too short.");
+			*nextsz = 0;
+			return;
+		}
+		addr = (sockaddr_in*)&state[1];
+	}
+	else
+	{ // initial
+		if(statemaxsz < sizeof(sockaddr_in) + 1)
+ 		{
+			LOG(ERROR) << _("ipv4_generator: state buffer is too short.");
+			*nextsz = 0;
+			return;
+		}
+		while(state[colon_pos] != ':' && colon_pos < *statesz)
+			++colon_pos;
+		// we don't need to convert in network byte order
+		char * state_cp = new char[*statesz];
+		memset(state_cp, 0, *statesz);
+		memcpy(state_cp, state, *statesz);
+		state[0] = 0x02;
+		addr = (sockaddr_in*)&state[1];
+		addr->sin_port = (unsigned short)atoi(&state_cp[colon_pos+1]);
+		state_cp[colon_pos] = '\0';
+		if(inet_pton(AF_INET, state_cp, &addr->sin_addr) <= 0)
+		{
+			LOG(ERROR) << _("ipv4_generator: inet_pton error.") << " "
+				<< _("Message") << ": " << strerror(errno);
+			nextsz = 0;
+			return;
+		}
+		delete [] state_cp;
+	}
+	*statesz = 1 + sizeof(sockaddr_in);
+
 	IPv4Info addr_info;
-	do{
-		if(addr.sin_addr.s_addr == inet_addr("223.255.255.255"))
+	addr_info = GetIPv4Info(addr->sin_addr.s_addr);
+	while(addr_info.addr_type != IPv4_ADDRTYPE_HOST 
+		|| addr_info.net_type == IPv4_NETTYPE_LOCAL 
+		|| addr_info.net_type == IPv4_NETTYPE_UNKNOWN)
+	{
+		if(ntohl(addr->sin_addr.s_addr) >= ntohl(inet_addr("223.255.255.255")))
 		{
 			if(repeat)
-			{
-				addr.sin_addr.s_addr = inet_addr("1.0.0.1");
+			{       	
+				addr->sin_addr.s_addr = inet_addr("1.0.0.1");
 			}
 			else
 			{
-				nextsz = 0;
+				state[0] = 0x03;
+				*statesz = 1;
 				return;
 			}
 		}
 		else
 		{
-			addr.sin_addr.s_addr = htonl(ntohl(addr.sin_addr.s_addr) + 1);
+			addr->sin_addr.s_addr = htonl(ntohl(addr->sin_addr.s_addr) + 1);
 		}
-		addr_info = GetIPv4Info(addr.sin_addr.s_addr);
-	} while(addr_info.addr_type != IPv4_ADDRTYPE_HOST 
-			&& addr_info.net_type != IPv4_NETTYPE_LOCAL 
-			&& addr_info.net_type != IPv4_NETTYPE_UNKNOWN);
-
-	if(inet_ntop())
-	{
+		addr_info = GetIPv4Info(addr->sin_addr.s_addr);
 	}
+
+	char tmp[50];
+	memset(tmp, 0, sizeof(tmp));
+	if(inet_ntop(AF_INET, &addr->sin_addr, tmp, sizeof(tmp) - 1) == NULL)
+	{
+		*nextsz = 0;
+		return;
+	}
+	if(nextmaxsz < strlen(tmp))
+	{
+		LOG(ERROR) << _("ipv4_generator: output buffer is too small.");
+		nextsz = 0;
+		return;
+	}
+	memcpy(next, tmp, strlen(tmp));
+	*nextsz = strlen(tmp);
+
+	do{
+		if(ntohl(addr->sin_addr.s_addr) >= ntohl(inet_addr("223.255.255.255")))
+		{
+			if(repeat)
+			{       	
+				addr->sin_addr.s_addr = inet_addr("1.0.0.1");
+			}
+			else
+			{
+				state[0] = 0x03;
+				*statesz = 1;
+				return;
+			}
+		}
+		else
+		{
+			addr->sin_addr.s_addr = htonl(ntohl(addr->sin_addr.s_addr) + 1);
+		}
+		addr_info = GetIPv4Info(addr->sin_addr.s_addr);
+	} while(addr_info.addr_type != IPv4_ADDRTYPE_HOST 
+		|| addr_info.net_type == IPv4_NETTYPE_LOCAL 
+		|| addr_info.net_type == IPv4_NETTYPE_UNKNOWN);
 }
 
 ///////////////////////////////////////////////////////////////////////////
@@ -203,7 +328,6 @@ CursorGenerator::CursorGenerator(const Cursor::Sockaddr addr, const Cursor::Args
 			}
 			memcpy(state_, init_utf8.data(), init_utf8.length());
 			statesz_ = init_utf8.length();
-			initial_ = arg.second;
 		}
 		else if(arg.first == "name")
 		{
@@ -211,6 +335,10 @@ CursorGenerator::CursorGenerator(const Cursor::Sockaddr addr, const Cursor::Args
 			if(name == "int")
 			{
 				generator = simple_uint_generator;
+			}
+			else if(name == "ipv4")
+			{
+				generator = ipv4_generator;
 			}
 			else
 			{
@@ -240,12 +368,6 @@ int CursorGenerator::Next(const size_t count, std::deque<nx::String>& buf)
 	if(generator == NULL)
 		return 0;
 	size_t i = 0;
-	if(!initial_.empty())
-	{
-		buf.push_back(initial_);
-		initial_.clear();
-		++i;
-	}
 	for(; i < count; ++i)
 	{
 		generator(state_, &statesz_, statemaxsz_, 
