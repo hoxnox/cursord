b8410609fe90a928449bfbcfcb9a569aca4421ab
N Yura <nyura@concerteza.ru>
2013-02-13 15:00:16 +0400

    documenting

---
+++ doc/index.h
@@ -0,0 +1,23 @@
+﻿////////////////////////////////////////////////////////////////////////////
+// index.h
+// Author: $username$ <$usermail$>
+// Licanse: Cursord license
+////////////////////////////////////////////////////////////////////////////
+
+/**
+
+@image html main_logo.png
+@author $username$ <$usermail$>
+@date $date$
+
+@mainpage cursord
+
+@tableofcontents
+
+@section intro Введение
+
+@subsection desc Детальное описание
+
+@li @subpage pagename
+
+*/
